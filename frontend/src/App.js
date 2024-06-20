0183274e9f84a6f9e01d880d68a9203d85ae0b57
mistirnigusse <mistirnigusse00@gmail.com>
2024-06-20 19:44:16 +0300

    feat: signup and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import BacktestForm from './components/BacktestForm';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <Navbar />
       <div className="container mx-auto p-4">
-        <BacktestForm />
+      <Routes>
+            <Route path="/" element={<BacktestForm />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
       </div>
     </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,65 @@
+// src/components/Login.js
+import React, { useState } from 'react';
+
+function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // Handle login
+  };
+
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">username</span>
+            </label>
+            <input
+              type="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-4">
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
