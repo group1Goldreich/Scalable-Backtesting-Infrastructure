ef1a268da91a305a176ecd8fd2cf5015cf05f9ec
GetachewAbebeA <gechachin@gmail.com>
2024-06-22 00:20:12 +0300

    CCI Strategy Added to Main Script

--- backtest/main.py
+++ backtest/main.py
@@ -3,6 +3,7 @@ from ema_strategy import EmaStrategy
 from rsi_strategy import RsiStrategy
 from macd_strategy import MacdStrategy
 from adx_strategy import AdxStrategy
+from cci_strategy import CciStrategy
 from backtest import run_backtest
 
 def main(strategy_name, start_date, end_date, params, start_cash, comm):
@@ -12,7 +13,8 @@ def main(strategy_name, start_date, end_date, params, start_cash, comm):
         'ema': EmaStrategy,
         'rsi': RsiStrategy,
         'macd': MacdStrategy,
-        'adx': AdxStrategy
+        'adx': AdxStrategy,
+        'cci': CciStrategy
     }
 
     if strategy_name in strategy_map:
