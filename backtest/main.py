b5413892a72e2e062b590414d085b48263fd0da6
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-21 23:11:06 +0300

    Removed the logger

--- backtest/main.py
+++ backtest/main.py
@@ -2,9 +2,6 @@ from sma_strategy import SmaStrategy
 from ema_strategy import EmaStrategy
 from rsi_strategy import RsiStrategy
 from backtest import run_backtest
-from logger import setup_logger
-
-logger = setup_logger('MainLogger')
 
 def main(strategy_name, start_date, end_date, params, start_cash, comm):
 
@@ -18,13 +15,8 @@ def main(strategy_name, start_date, end_date, params, start_cash, comm):
         strategy = strategy_map[strategy_name]
         results = run_backtest(strategy, params, 'data/BTC-USD.csv', start_date, end_date, start_cash, comm)
 
-        logger.info(f"Final Portfolio Value: {results['final_value']}")
-        logger.info(f"Trade Analyzer: {results['trade_analyzer']}")
-        logger.info(f"Drawdown: {results['drawdown']}")
-        logger.info(f"Sharpe Ratio: {results['sharperatio']}")
-
     else:
-        logger.error(f"Strategy {strategy_name} is not recognized.")
+         print(f"Strategy {strategy_name} is not recognized.")
 
 if __name__ == '__main__':
     main('rsi', '2023-06-18', '2024-06-18', {'rsi_period':15, 'oversold':30, 'overbought':70}, 1000000, 0.001)
