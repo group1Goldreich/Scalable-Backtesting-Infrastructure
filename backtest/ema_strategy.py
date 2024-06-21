683c0c2cfe372d2d2394b6d8ac3e2d1bb7fdf97f
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-21 22:54:10 +0300

    Added the log

--- backtest/ema_strategy.py
+++ backtest/ema_strategy.py
@@ -4,7 +4,6 @@ import os.path
 import backtrader as bt
 from logger import setup_logger
 
-logger = setup_logger('SmaStrategyLogger')
 
 class EmaStrategy(bt.Strategy):
 
@@ -14,48 +13,60 @@ class EmaStrategy(bt.Strategy):
         ('comm', 0.0),
     )
 
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
     def __init__(self):
         self.dataclose = self.datas[0].close
         self.order = None
         self.buyprice = None
 
-        self.smashort = bt.indicators.ExponentialMovingAverage(self.datas[0], period=self.params.short_period)
-        self.smalong = bt.indicators.ExponentialMovingAverage(self.datas[0], period=self.params.long_period)
+        self.emashort = bt.indicators.ExponentialMovingAverage(self.datas[0], period=self.params.short_period)
+        self.emalong = bt.indicators.ExponentialMovingAverage(self.datas[0], period=self.params.long_period)
 
     def notify_order(self, order):
+
         if order.status in [order.Submitted, order.Accepted]:
             return
 
         if order.status in [order.Completed]:
             if order.isbuy():
+                self.log('Buy created Price: %.2f, Cost: %.2f, Commission: %.2f'%
+                     (order.executed.price,
+                      order.executed.value,
+                      order.executed.comm))
+                
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
-                logger.info(f'BUY EXECUTED, Price: {self.buyprice}, Cost: {order.executed.value}, Comm: {self.buycomm}')
             else:
+                self.log('Sell created Price: %.2f, Cost: %.2f, Commission: %.2f'%
+                     (order.executed.price,
+                      order.executed.value,
+                      order.executed.comm))
                 self.bar_executed = len(self)
-                logger.info(f'SELL EXECUTED, Price: {order.executed.price}, Cost: {order.executed.value}, Comm: {order.executed.comm}')
-
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            logger.warning('Order Canceled/Margin/Rejected')
+            self.order('Order Cancled/margin/rejected')
             self.order = None
 
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
-        logger.info(f'TRADE PROFIT, GROSS {trade.pnl}, NET {trade.pnlcomm}')
+        self.log(f'TRADE PROFIT, GROSS {trade.pnl}, NET {trade.pnlcomm}')
 
     def next(self):
+        self.log('Close, %.2f' % self.dataclose[0])
         if self.order:
             return
 
         if not self.position:
-            if self.smashort[0] > self.smalong[0]:
+            if self.emashort[0] > self.emalong[0]:
+                self.log('Buy Create, %.2f' % self.dataclose[0])
                 self.order = self.buy()
-                logger.info(f'BUY CREATE, {self.dataclose[0]}')
         else:
-            if self.smashort[0] < self.smalong[0]:
+            if self.emashort[0] < self.emalong[0]:
+                self.log('Sell Create, %.2f' % self.dataclose[0])
                 self.order = self.sell()
-                logger.info(f'SELL CREATE, {self.dataclose[0]}')
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
@@ -69,6 +80,6 @@ if __name__ == '__main__':
     )
     cerebro.adddata(data)
     cerebro.broker.setcash(100000)
-    logger.info(f"Starting Portfolio Value: {cerebro.broker.getvalue()}")
+    print(f"Starting Portfolio Value: {cerebro.broker.getvalue()}")
     cerebro.run()
-    logger.info(f"Ending Portfolio Value: {cerebro.broker.getvalue()}")
+    print(f"Ending Portfolio Value: {cerebro.broker.getvalue()}")
