198feb044d08cc84c71d501a5a5404a3b99f3ffe
eyerusalem <eyerusad12@gmail.com>
2024-06-22 04:48:27 +0300

    model renamed

--- backend/models/indicator.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database_connection import Base
-
-class Indicator(Base):
-    
-    __tablename__ = 'indicators'
-    indicator_id = Column(Integer, primary_key=True, index=True)
-    indicator_name = Column(String)
-    indicator_params = Column(String)
