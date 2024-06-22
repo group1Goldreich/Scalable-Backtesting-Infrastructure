1eeec7a944887004444014cebb86dbab3c5ecc02
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-22 17:34:14 +0300

    Script for consumer

---
+++ kafka_scripts/kafka_consumer.py
@@ -0,0 +1,35 @@
+from kafka import KafkaConsumer
+import json
+import sys
+import os
+
+def consume_backtest_request():
+    consumer = KafkaConsumer(
+        'backend_requests',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='latest',
+        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+    )
+
+    for message in consumer:
+        start_date = message.value['start_date']
+        end_date = message.value['end_date']
+        strategy = message.value['strategy']
+        params = message.value['params']
+        start_cash = message.value['start_cash']
+        comm = message.value['comm']
+
+        yield strategy, start_date, end_date, params, start_cash, comm
+
+
+def consume_backtest_results():
+    consumer = KafkaConsumer(
+        'backtest_results',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='latest',
+        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+    )
+
+    for message in consumer:
+        metrics = message.value['metrics']
+        return metrics
