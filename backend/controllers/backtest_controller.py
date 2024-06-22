08bd0dc93ff693998d32a7631c67636ecb64308c
eyerusalem <eyerusad12@gmail.com>
2024-06-22 15:14:17 +0300

    db_backtest_result removed

--- backend/controllers/backtest_controller.py
+++ backend/controllers/backtest_controller.py
@@ -3,30 +3,15 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from ..models.backtest_result_model import BacktestResult
 from ..models.scene_model import Scene
-from ..backtest.sma_strategy import SmaStrategy
-from ..backtest.ema_strategy import EmaStrategy
-from ..backtest.rsi_strategy import RsiStrategy
-from ..backtest import run_backtest
 from ..models.indicator_model import Indicator
 from ..view_models.scenes_vm import ScenesBaseVM
-from ..backtest.logger import setup_logger
 
 
-logger = setup_logger('MainLogger')
+
 
 def backtest(db: Session, data: ScenesBaseVM):
     try:
-        strategy_map = {
-            'sma': SmaStrategy,
-            'ema': EmaStrategy,
-            'rsi': RsiStrategy
-        }
-
-        if data.strategy_name in strategy_map:
-            strategy_class = strategy_map[data.strategy_name]
-            results = run_backtest(strategy_class, data.params, './backtest/data/BTC-USD.csv', data.start_date, data.end_date, data.start_cash, data.commission)
-
-            # Create Indicator record
+        
             db_indicator = Indicator(
                 indicator_name=data.strategy_name,
                 indicator_params=data.params
@@ -50,28 +35,37 @@ def backtest(db: Session, data: ScenesBaseVM):
             db.flush()  # Ensure the scene gets an ID before it's used in BacktestResult
 
             # Create BacktestResult record
-            db_backtest_result = BacktestResult(
-                scene=db_scene,  # Assign the scene object to backtest_result
-                final_portfolio_value=results['final_portfolio_value'],
-                total_trades=results['total_trades'],
-                winning_trades=results['winning_trades'],
-                losing_trades=results['losing_trades'],
-                max_drawdown=results['max_drawdown'],
-                max_moneydown=results['max_moneydown'],
-                sharpe_ratio=results['sharpe_ratio']
-            )
+            # db_backtest_result = BacktestResult(
+            #     scene=db_scene,  # Assign the scene object to backtest_result
+            #     final_portfolio_value=data['final_portfolio_value'],
+            #     total_trades=results['total_trades'],
+            #     winning_trades=results['winning_trades'],
+            #     losing_trades=results['losing_trades'],
+            #     max_drawdown=results['max_drawdown'],
+            #     max_moneydown=results['max_moneydown'],
+            #     sharpe_ratio=results['sharpe_ratio']
+            # )
+            
+            # db_backtest_result = BacktestResult(
+            #     scene=db_scene,  # Assign the scene object to backtest_result
+            #     final_portfolio_value=data.final_portfolio_value,
+            #     total_trades= data.total_trades,
+            #     winning_trades=data.winning_trades,
+            #     losing_trades=data.losing_trades,
+            #     max_drawdown=data.max_drawdown,
+            #     max_moneydown=data.max_moneydown,
+            #     sharpe_ratio=data.sharpe_ratio
+            # )
 
             # Add BacktestResult to session
-            db.add(db_backtest_result)
+            # db.add(db_backtest_result)
             db.commit()  # Commit all changes to the database
 
-            return db_backtest_result
+            return db_scene
 
-        else:
-            logger.error(f"Strategy {data.strategy_name} is not recognized.")
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Strategy {data.strategy_name} is not recognized.")
+       
 
     except SQLAlchemyError as e:
         db.rollback()
-        logger.error(f"Database error occurred: {e}")
+      
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error occurred.")
