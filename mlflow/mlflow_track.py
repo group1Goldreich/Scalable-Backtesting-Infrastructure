be797ff251a8290bb4714b08c2af30588a39ac38
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-23 19:58:05 +0300

    Loged the name of the coin

--- mlflow/mlflow_track.py
+++ mlflow/mlflow_track.py
@@ -4,9 +4,10 @@ import mlflow.pyfunc
 mlflow.set_tracking_uri("http://localhost:5001")
 
 
-def track(strategy, start_date, end_date, start_cash, commission, params, metrics):
+def track(name, strategy, start_date, end_date, start_cash, commission, params, metrics):
 
         with mlflow.start_run():
+            mlflow.log_param('Coin Name', name)
             mlflow.log_param('Strategy', strategy)
             mlflow.log_param('Start date', start_date)
             mlflow.log_param('End date', end_date)
