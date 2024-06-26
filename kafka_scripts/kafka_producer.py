5170780648f467528abcf720362cf665bbb9a771
Yohanes Teshome Kebede <99422479+Yohanes213@users.noreply.github.com>
2024-06-26 07:19:38 +0000

    Modified file

--- kafka_scripts/kafka_producer.py
+++ kafka_scripts/kafka_producer.py
@@ -1,6 +1,28 @@
+from kafka import KafkaAdminClient
+from kafka.admin import NewTopic
 import json
 from kafka import KafkaProducer
 
+# Define Kafka connection parameters
+#bootstrap_servers = 'kafka:9092' 
+
+# # Create admin client
+# admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+
+# # Define topics to create
+# topics = ['backend_requests', 'backtest_results']
+
+# # Check if topics exist, create them if not
+# existing_topics = admin_client.list_topics()
+# for topic in topics:
+#     if topic not in existing_topics:
+#         new_topic = NewTopic(name=topic, num_partitions=1, replication_factor=1)
+#         admin_client.create_topics(new_topics=[new_topic])
+
+# # Close admin client
+# admin_client.close()
+
+# Create Kafka producer
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 def send_backend_request(name, start_date, end_date, strategy, params, start_cash, comm):
@@ -13,13 +35,14 @@ def send_backend_request(name, start_date, end_date, strategy, params, start_cas
         'start_cash': start_cash,
         'comm': comm
     }
+
+    # Produce message to backend_requests topic
     producer.send('backend_requests', value=message)
     producer.flush()
 
-
+# Define function to send backtest results
 def send_backtest_results(metrics):
     message = {
-        #'backtest_id': backtest_id,
         'metrics': metrics
     }
     producer.send('backtest_results', value=message)
